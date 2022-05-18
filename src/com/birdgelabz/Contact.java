f03bff00b8e7c941108931250cc75474a99ee39f
SwatiBadole <swatibadole1997@gmail.com>
2022-05-18 11:07:02 -0700

    Added :Contact in AddressBook

---
+++ src/com/birdgelabz/Contact.java
@@ -0,0 +1,12 @@
+package com.birdgelabz;
+
+public class Contact {
+     String firstName;
+     String lastName;
+     String address;
+     String city;
+     String state;
+     int zip;
+     long phone;
+     String email;
+}
