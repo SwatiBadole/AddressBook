ff305e043d71b8ac82b719cc6d59ab9cd8333c86
SwatiBadole <swatibadole1997@gmail.com>
2022-05-18 11:22:25 -0700

    Added:Ability to add new contact to AdderssBook

--- src/com/birdgelabz/Contact.java
+++ src/com/birdgelabz/Contact.java
@@ -9,4 +9,20 @@ public class Contact {
      int zip;
      long phone;
      String email;
+
+     public Contact(String firstName,String lastName,String address,String city,String state,int zip,long phone,String email)
+     {
+          super();
+          this.firstName=firstName;
+          this.lastName=lastName;
+          this.address=address;
+          this.city=city;
+          this.state=state;
+          this.zip=zip;
+          this.email=email;
+     }
 }
+
+
+
+
